be31dd5d841225fa21e3d4185eb3f22bb2c74563
Shivasai-psy <shivasaijangaml.c@gmail.com>
2026-07-20 14:58:01 +0530

    Create get_response function for user input handling
    Refactored chatbot interaction into a separate function.

--- main.py
+++ main.py
@@ -5,23 +5,19 @@ client = genai.configure(api_key = API_KEY)
 
 print('--------🤖AI CHAT ASSISTANT🤖---------')
 
-while True:
-    user_input = input('You: ')
-    if user_input.lower() in ["quit","exit","bye"]:
-        print('\n 🤖 Chat bot: Goodbye!')
-        break
-    
-    response = client.models.generate_content_stream(
-        model = 'gemini-3.5-flash',
-        message = [
-            {"role":"system","content":"you are helpful chatbot"},
-            {"role":"user","content":user_input}
-        ],
-        contents = True
-    )
-
-    print('🤖chatbot: ',end='',flush=True)
-
-    for chunk in response:
-        print(chunk.text,end='',flush=True)
+def get_response():
+    while True:
+        user_input = input('You: ')
+        if user_input.lower() in ["quit","exit","bye"]:
+            print('\n 🤖 Chat bot: Goodbye!')
+            break
+            response = client.models.generate_content_stream(),
+            model = 'gemini-3.5-flash',
+            message = [
+                {"role":"system","content":"you are helpful chatbot"},
+                {"role":"user","content":user_input}
+            ],
+            contents = True
+            )
+            return response.text
      
